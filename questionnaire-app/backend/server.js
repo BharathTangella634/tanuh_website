e00fc919e18367c0e08b7cf35cdac9a7501acb3e
Bharath Kumar <tangellabharathkumar@gmail.com>
2025-10-28 16:19:02 +0530

    backend check

--- questionnaire-app/backend/server.js
+++ questionnaire-app/backend/server.js
@@ -182,7 +182,8 @@ import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
 import { getPool } from '../mysql_explorer/db.js';
 
-import questionnaireData from '../src/assets/questionnaire.json' with { type: 'json' };
+// import questionnaireData from '../src/assets/questionnaire.json' with { type: 'json' };
+import questionnaireData from './questionnaire.json' with { type: 'json' };
 
 const app = express();
 const PORT = process.env.PORT || 3001;
